8ace3f7d53c0433abaf75fff6f5eef9eb4c12b5f
Antoine Solé <antoine.sole@epita.fr>
2022-03-19 16:27:01 +0100

    drivers: gistre: gistre_card: commands: return when error

--- drivers/gistre/gistre_card/commands/mem_write.c
+++ drivers/gistre/gistre_card/commands/mem_write.c
@@ -51,9 +51,9 @@ int process_write(const struct command *command, const struct regmap *regmap,
 		data_size = INTERNAL_BUFFER_SIZE;
 	}
 	if (data_size > given_size) {
-		LOG("write: asking to write %d but only %d were given, filling with zeroes",
-			LOG_EXTRA, mfrc522_driver_dev->log_level, data_size, given_size);
-		data_size = given_size;
+		LOG("write: asking to write %d but only %d were given",
+			LOG_ERROR, mfrc522_driver_dev->log_level, data_size, given_size);
+		return -1;
 	}
 
 	if (flush_fifo(regmap, mfrc522_driver_dev->log_level) < 0)
